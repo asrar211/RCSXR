e61bae838dd11369821ea2c3f5787d73d54daf6b
Asrar <113281948+asrar211@users.noreply.github.com>
2025-11-07 19:02:54 +0530

    new

--- src/components/Home/FeaturedProducts.tsx
+++ src/components/Home/FeaturedProducts.tsx
@@ -43,7 +43,7 @@ export const FeaturedProducts = () => {
 
         <div className="flex justify-center items-center">
   <button className="bg-white px-6 py-3.5 rounded-md shadow-sm ring-1 ring-neutral-200 text-[14px] hover:bg-neutral-100 transition-all duration-300 ease-in-out w-fit flex gap-2 items-center justify-center group hover:shadow-lg">
-    View All Products <span className="group-hover:translate-x-1.5 transition-all translate-y-0.5 duration-300 ease-in-out"><IconArrowRight size={16}/></span>
+    View All Products <span className="group-hover:translate-x-1.5 transition-all duration-300 ease-in-out"><IconArrowRight size={16}/></span>
   </button>
 </div>
     </div>
